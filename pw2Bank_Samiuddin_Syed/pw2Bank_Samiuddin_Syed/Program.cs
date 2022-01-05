1d9fe5f0cadad81f9830163c75b2ae4e68dc1d50
Nisarg Prajapati <nisargprajapati24@gmail.com>
2022-01-05 15:32:06 -0500

    Bank simulation in C#

---
+++ pw2Bank_Samiuddin_Syed/pw2Bank_Samiuddin_Syed/Program.cs
@@ -0,0 +1,186 @@
+﻿
+using pw2Bank_Samiuddin_Syed.classes;
+using System;
+using System.Linq;
+
+/*
+ * Team members
+ * 
+    Abhiroop Singh Abhiroop Singh
+    Danish Danish
+    Nisarg Kumar Ashokbhai Prajapati
+    Deep ParkashKumar Vagehla
+    Syed, Samiuddin
+    Waghmare, Abhijeet Shekhar
+ 
+ */
+namespace pw2Bank_Samiuddin_Syed
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int choice = -1;
+
+            do
+            {
+                Console.WriteLine("\n\tMenu\n=======================\n1. Add a bank account" +
+                    "\n2. Remove a bank account(by Account Number)" +
+                    "\n3. Display Client's information(by Account Number)" +
+                    "\n4. Deposit to particular account(by Account Number)" +
+                    "\n5. Withdrawal from particular account(by Account Number)" +
+                    "\n6. Sort and Display clients information" +
+                    "\n7. Display average balance of accounts" +
+                    "\n8. Display total balance of accounts" +
+                    "\n9. Exit");
+                try
+                {
+                    Console.Write("\nEnter your choice: ");
+                    choice = Convert.ToInt32(Console.ReadLine());
+
+                    switch (choice)
+                    {
+                        case 1:
+                            //Add Account
+                            addAccount();
+                            break;
+                        case 2:
+                            //Remove account by account num
+                            removeAccount();
+                            break;
+                        case 3:
+                            //Display single account information by account number
+                            Console.Write("\nEnter Client's account number: ");
+                            long accNum = Convert.ToInt64(Console.ReadLine());
+
+                            Console.WriteLine("\nClients Information\n========================");
+                            Console.WriteLine("\n" + Bank.DisplayInfo(accNum));
+                            break;
+                        case 4:
+                            //Deposit
+                            deposit();
+                            break;
+                        case 5:
+                            //Withdraw
+                            withdraw();
+                            break;
+                        case 6:
+                            //Sort and Display
+                            Bank.sortAccountsInAsc();
+                            displayAllAccountsInfo();
+                            break;
+                        case 7:
+                            //average balance of accounts
+                            Console.WriteLine("Average balance of accounts: "
+                                + Math.Round(Bank.GetAverageBalanceAccounts(), 6));
+                            break;
+                        case 8:
+                            //Total balance of accounts
+                            Console.WriteLine("Total balance of accounts: "
+                                    + Math.Round(Bank.GetTotalBalanceAccounts(), 6));
+                            break;
+                        case 9:
+                            Console.WriteLine("Goodbye!");
+                            break;
+                        default:
+                            Console.WriteLine(">> Invalid Choice! Try again! <<");
+                            break;
+                    }
+                } catch(Exception e)
+                {
+                    Console.WriteLine("\n"+e.Message);
+                }
+                
+
+            } while (choice != 9);
+
+            Console.ReadLine();
+        }
+
+        public static void addAccount()
+        {
+            //Add Account
+            Console.Write("\nEnter Client's Given Name: ");
+            String givenName = Console.ReadLine();
+            Console.Write("Enter Client's Family Name: ");
+            String familyName = Console.ReadLine();
+
+            Client client = new Client(givenName, familyName);
+
+            Console.Write("Enter Client's Balance: ");
+            double balance = Convert.ToDouble(Console.ReadLine());
+
+            Account acc = new Account(balance, client);
+
+            if (Bank.AddAccount(acc, client))
+                Console.WriteLine("\nAccount added successfully!");
+            else
+                Console.WriteLine("\nAccounts limit reached(Only 100 accounts)");
+        }
+
+        public static void removeAccount()
+        {
+            Console.Write("\nEnter Client's account number: ");
+            long accNumber = Convert.ToInt64(Console.ReadLine());
+
+            if (Bank.RemoveAccount(accNumber))
+                Console.WriteLine("\nClient's account is deleted!");
+            else
+                Console.WriteLine("\nNo account found!");
+        }
+
+        public static void deposit()
+        {
+            Console.Write("\nEnter Client's account number: ");
+            long acctNum = Convert.ToInt64(Console.ReadLine());
+            Console.Write("\nEnter amount to deposit: ");
+            double amount = Convert.ToDouble(Console.ReadLine());
+
+            if (Bank.Deposit(acctNum, amount))
+                Console.WriteLine("\nAmount Deposited successfully");
+            else
+                Console.WriteLine("\nNo account found!");
+        }
+
+        public static void withdraw()
+        {
+            Console.Write("\nEnter Client's account number: ");
+            long atNum = Convert.ToInt64(Console.ReadLine());
+
+            Console.Write("\nEnter amount to withdraw: ");
+            double withAmt = Convert.ToDouble(Console.ReadLine());
+
+            /**
+            * returns -1 for account not found
+            * returns -2 for insufficient balance
+            * returns >= 0 for success
+            */
+            int status = Bank.Withdraw(atNum, withAmt);
+
+            if (status > 0)
+                Console.WriteLine("Withdraw successfully!");
+            else if (status == -1)
+                Console.WriteLine("\nNo account found!");
+            else if (status == -2)
+                Console.WriteLine("\nInsufficient balance!");
+        }
+
+
+
+        public static void displayAllAccountsInfo()
+        {
+            if (Bank.Accounts.Count() > 0)
+            {
+                Console.WriteLine("\nClients Information");
+                foreach (Account aacc in Bank.Accounts)
+                {
+                    Console.WriteLine("=====================================");
+                    Console.WriteLine(Bank.DisplayInfo(aacc.AccountNumber));
+                }
+            }
+            else
+                Console.WriteLine("\nList is empty!");
+        }
+    }
+}
---
+++ pw2Bank_Samiuddin_Syed/pw2Bank_Samiuddin_Syed/classes/Bank.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pw2Bank_Samiuddin_Syed.classes
+{
+    class Bank
+    {
+        private static List<Account> accounts = new List<Account>();
+
+        internal static List<Account> Accounts { get => accounts; }
+
+        public static bool AddAccount(Account acc, Client client)
+        {
+            Random rand = new Random();
+            long randAccNum;
+
+            do
+            {
+                //10000 - 10099 
+                randAccNum = rand.Next(10000, 10099);
+
+            } while(GetIndexOfAccount(randAccNum) >= 0); //if account number available then generate another random num
+            
+            //only 100 accounts can be added
+            if (Accounts.Count < 100)
+            {
+                Account account = new Account(randAccNum, acc.Balance, client);
+                Accounts.Add(account);
+                return true;
+            }
+
+            return false;
+            
+        }
+
+        
+
+        public static bool RemoveAccount(long accNum)
+        {
+            //gets index of the account in list or else return -1
+            int indx = GetIndexOfAccount(accNum);
+            if (indx >= 0)
+            {
+                Accounts.RemoveAt(indx);
+                return true;
+            }
+                
+            return false;
+        }
+
+        public static int GetIndexOfAccount(long accNum)
+        {
+            //gets index of the account in list or else return -1
+            foreach(Account acc in Accounts)
+            {
+                if (acc.AccountNumber.Equals(accNum))
+                {
+                    return Accounts.IndexOf(acc);
+                }
+            }
+            return -1;
+        }
+        public static bool Deposit(long accountNumber, double depositBalance)
+        {
+            //get index of account in list
+            int indx = GetIndexOfAccount(accountNumber);
+
+            if (indx >= 0)
+            {
+                Accounts[indx].Balance = Accounts[indx].Balance + depositBalance;
+                return true;
+            }
+
+            return false;
+        }
+
+        /**
+         * returns -1 for account not found
+         * returns -2 for insufficient balance
+         * returns >= 0 for success
+         */
+        public static int Withdraw(long accountNumber, double withdrawBalance)
+        {
+            int indx = GetIndexOfAccount(accountNumber);
+
+            //checking if account is available
+            if (indx >= 0)
+            {
+                double availableBalance =  Accounts[indx].Balance;
+
+                if (availableBalance >= withdrawBalance)
+                {
+                    Accounts[indx].Balance = Accounts[indx].Balance - withdrawBalance;
+                    return 1;
+                }
+                else
+                {
+                    return -2;
+                }
+            }
+
+            return -1;
+        }
+        public static void sortAccountsInAsc()
+        {
+            //sorting by balance then familyname, then given name,in ascending order
+            Accounts.OrderBy(ac => ac.Balance).ThenBy(ac => ac.Client.FamilyName)
+                            .ThenBy(ac => ac.Client.GivenName).ToList();
+        }
+
+        public static String DisplayInfo(long accountNumber)
+        {  
+           
+            foreach (Account acc in Accounts)
+            {
+                if (acc.AccountNumber.Equals(accountNumber))
+                {
+                    return "\nAccount Number: "+acc.AccountNumber + "\nGiven Name: " +acc.Client.GivenName
+                        +"\nFamily Name: "+ acc.Client.FamilyName + "\nBalance: " + acc.Balance;
+                    
+                }
+            }
+            return "\nNo account found!";  
+        }
+
+        public static double GetAverageBalanceAccounts()
+        {
+            int accountsCount = Accounts.Count;
+
+            return GetTotalBalanceAccounts() / accountsCount;
+        }
+
+        public static double GetTotalBalanceAccounts()
+        {
+            double sum = 0;
+   
+            foreach (Account accs in Accounts)
+                sum = sum + accs.Balance;
+
+            return sum;
+        }
+
+    }
+}
